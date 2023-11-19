99fa6282a0af755f4f2782ce95edc7fbb8894ff2
MariaDimova <150257957+MariaDimova@users.noreply.github.com>
2023-11-19 17:54:15 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greeting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // “Hello, <name>!”, където <name> е въведеното преди това име.
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + '!');
+
+        }
+    }
+}
